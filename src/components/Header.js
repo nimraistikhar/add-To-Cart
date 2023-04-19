cf4f9cb02fbcd61416c353282aef32ff645db3ad
nimra <nimra@myyogateacher.com>
2023-04-19 21:47:54 +0500

    style Card details page and make some redux files

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './styles.css'
+import { NavLink } from 'react-router-dom';
+import { Menu } from '@mui/material';
+import { Close } from '@mui/icons-material';
+
+function Header() {
+
+    const [anchorE1, setAnchorE1] = useState(null);
+    const open = Boolean(anchorE1);
+    const handleClick = (event) => {
+        setAnchorE1(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorE1(null)
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/"><span>Add to cart</span></NavLink></li>
+                        <li><NavLink to="/">Home</NavLink></li>
+                    </ul>
+                    <div className='cartIcon'
+                        id='basic-button'
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        <ShoppingCartIcon style={{ fontSize: 50 }} />
+                        <span>0</span>
+                    </div>
+                </nav>
+            </div>
+
+            <Menu
+                id='basic-menu'
+                anchorEl={anchorE1}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <div className='card_details'>
+                    <div className='close' onClick={handleClose}>
+                        <Close />
+                    </div>
+                    <p>Your card is empty</p>
+                </div>
+            </Menu>
+        </>
+    )
+}
+
+export default Header
--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,4 +1,3 @@
 const INIT_STATE = {
     carts: []
 };
-
